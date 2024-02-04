1af6e93933e1c44e9bd3233d7c7b8ad92166399d
bon jae ku <scottku@naver.com>
2024-02-04 22:27:17 +0900

    24.02.04 백준 문제풀이 11729 : 횟수를 확인해 보던 중, 원판이 4개라면 3개를 2번째 칸에 옮기고 4번째 원판을 3번째 칸으로 옮긴 후 다시 3개를 3번째 칸으로 옮기는 작업을 진행하는 것을 파악했다. 즉 n개를 옮기는 횟수 dp[n]은 dp[n-1] + 1 + dp[n-1] 이었고, 이에 따라 옮기는 순서를 출력하도록 재귀함수를 만들었다. 2447 : 쉬운 재귀함수 문제, 중간은 어차피 비어있으니 빈 부분은 제하고 9번의 부분반복이 아닌 8번의 부분 반복으로 풀이

---
+++ 24.02.04/q11729.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int n;
+
+void moveToThree(int start, int mid, int end, int num)
+{
+	if (num == 1)
+		printf("%d %d\n", start, end);
+	else
+	{
+		moveToThree(start, end, mid, num - 1);
+		printf("%d %d\n", start, end);
+		moveToThree(mid, start, end, num - 1);
+	}
+}
+
+int main(void)
+{
+	scanf("%d", &n);
+	// n-1개를 2번으로 옮기고 n 번째를 3으로 옮긴 후, 2번에서 3번으로 n-1을 옮기기
+	int _count = pow(2, n);
+	printf("%d\n", _count - 1);
+	moveToThree(1, 2, 3, n);
+}
---
+++ 24.02.04/q2447.cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int n;
+
+bool arr[6562][6562];
+
+void makePattern(int x_start, int y_start, int _size)
+{
+	for (int i = x_start + _size / 3; i < x_start + _size * 2 / 3; i++)
+	{
+		for (int j = y_start + _size / 3; j < y_start + _size * 2 / 3; j++)
+		{
+			arr[i][j] = false;
+		}
+	}
+	if (_size == 3)
+		return;
+	else
+	{
+		makePattern(x_start, y_start, _size / 3);
+		makePattern(x_start + _size / 3, y_start, _size / 3);
+		makePattern(x_start + _size * 2 / 3, y_start, _size / 3);
+		makePattern(x_start, y_start + _size / 3, _size / 3);
+		makePattern(x_start + 2 * _size / 3, y_start + _size / 3, _size / 3);
+		makePattern(x_start, y_start + 2 * _size / 3, _size / 3);
+		makePattern(x_start + _size / 3, y_start + 2 * _size / 3, _size / 3);
+		makePattern(x_start + 2 * _size / 3, y_start + 2 * _size / 3, _size / 3);
+	}
+}
+
+int main(void)
+{
+	memset(arr, true, sizeof(arr));
+	scanf("%d", &n);
+	makePattern(1, 1, n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (arr[i][j])
+				printf("%c", '*');
+			else
+				printf("%c", ' ');
+
+			if (j == n)
+				printf("\n");
+		}
+	}
+}
