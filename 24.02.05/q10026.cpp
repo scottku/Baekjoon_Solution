b5b84b5aefdfbfd83dbe92f42b728a8fd5622d2f
bon jae ku <scottku@naver.com>
2024-02-05 21:34:12 +0900

    24.02.05 백준 문제풀이 10026 : 무난한 BFS, 단지 귀찮은 조건이 하나 추가됐을 뿐 12865 : DP로 풀어야 하는 것은 인지, 무게 n일 때의 가중치 최댓값, 가중치 n일때의 무게 최솟값 방식으로 접근했으나, 특정 무게일때의 최댓값 + 특정 무게일때의 최댓값 시 똑같은 물건이 겹쳐서 들어오는 경우가 생겨 고민함. 이때는 무게나 가치를 기준이 아닌, 몇번째 물건까지 고려했을때 무게를 만족하면서 가치를 최대로 얻을 수 있는가로 생각했어야 했다. 고려해야될 부분이 2가지여서 2차 배열로 진행한 것도 변경점

---
+++ 24.02.05/q10026.cpp
@@ -0,0 +1,177 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int n;
+char arr[100][100];
+bool visit[100][100];
+bool visit_cb[100][100];
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+int _count = 0;
+
+void findArea(int x, int y, char c, bool b_cb, bool b_first)
+{
+	if (b_cb)
+	{
+		if (visit_cb[x][y])
+		{
+			if (y + 1 >= n)
+			{
+				if (x + 1 < n)
+					findArea(x + 1, 0, arr[x + 1][0], b_cb, true);
+			}
+			else
+				findArea(x, y + 1, arr[x][y + 1], b_cb, true);
+
+			return;
+		}
+
+		visit_cb[x][y] = true;
+		if (b_first) // 처음 시작 위치를 잡으려고 한다.
+		{
+			_count++;
+			for (int i = 0; i < 4; i++)
+			{
+				int _dx = x + dx[i]; // 위 아래
+				int _dy = y + dy[i]; // 양 옆
+				if ((int)c == (int)'R' || (int)c == (int)'G')
+				{
+					if (_dx >= 0 && _dx < n && _dy >= 0 && _dy < n && !visit_cb[_dx][_dy] && ((int)arr[_dx][_dy] == (int)'R' || (int)arr[_dx][_dy] == (int)'G'))
+					{
+						findArea(_dx, _dy, c, b_cb, false);
+					}
+				}
+				else // c == 'B'
+				{
+					if (_dx >= 0 && _dx < n && _dy >= 0 && _dy < n && !visit_cb[_dx][_dy] && (int)arr[_dx][_dy] == (int)c)
+					{
+						findArea(_dx, _dy, c, b_cb, false);
+					}
+				}
+			}
+
+			if (y + 1 >= n)
+			{
+				if (x + 1 < n)
+					findArea(x + 1, 0, arr[x + 1][0], b_cb, true);
+			}
+			else
+				findArea(x, y + 1, arr[x][y + 1], b_cb, true);
+		}
+		else
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				int _dx = x + dx[i]; // 위 아래
+				int _dy = y + dy[i]; // 양 옆
+				if ((int)c == (int)'R' || (int)c == (int)'G')
+				{
+					if (_dx >= 0 && _dx < n && _dy >= 0 && _dy < n && !visit_cb[_dx][_dy] && ((int)arr[_dx][_dy] == (int)'R' || (int)arr[_dx][_dy] == (int)'G'))
+					{
+						findArea(_dx, _dy, c, b_cb, false);
+					}
+				}
+				else // c == 'B'
+				{
+					if (_dx >= 0 && _dx < n && _dy >= 0 && _dy < n && !visit_cb[_dx][_dy] && (int)arr[_dx][_dy] == (int)c)
+					{
+						findArea(_dx, _dy, c, b_cb, false);
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		if (visit[x][y])
+		{
+			if (y + 1 >= n)
+			{
+				if (x + 1 < n)
+					findArea(x + 1, 0, arr[x + 1][0], b_cb, true);
+			}
+			else
+				findArea(x, y + 1, arr[x][y + 1], b_cb, true);
+
+			return;
+		}
+
+		visit[x][y] = true;
+		if (b_first) // 처음 시작 위치를 잡으려고 한다.
+		{
+			_count++;
+			for (int i = 0; i < 4; i++)
+			{
+				int _dx = x + dx[i]; // 위 아래
+				int _dy = y + dy[i]; // 양 옆
+				if (_dx >= 0 && _dx < n && _dy >= 0 && _dy < n && !visit[_dx][_dy] && (int)arr[_dx][_dy] == (int)c)
+				{
+					findArea(_dx, _dy, c, b_cb, false);
+				}
+			}
+
+			if (y + 1 >= n)
+			{
+				if (x + 1 < n)
+					findArea(x + 1, 0, arr[x + 1][0], b_cb, true);
+			}
+			else
+				findArea(x, y + 1, arr[x][y + 1], b_cb, true);
+		}
+		else
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				int _dx = x + dx[i]; // 위 아래
+				int _dy = y + dy[i]; // 양 옆
+				if (_dx >= 0 && _dx < n && _dy >= 0 && _dy < n && !visit[_dx][_dy] && (int)arr[_dx][_dy] == (int)c)
+				{
+					findArea(_dx, _dy, c, b_cb, false);
+				}
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	memset(visit, false, sizeof(visit));
+	memset(visit_cb, false, sizeof(visit_cb));
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			char t;
+			scanf("%c", &t);
+			if ((int)t == (int)'\n')
+			{
+				j -= 1;
+				continue;
+			}
+			arr[i][j] = t;
+		}
+	}
+
+	findArea(0, 0, arr[0][0], false, true); // x, y, 문자, 색약인가?, 첫 시작점인가?
+	printf("%d ", _count);
+	_count = 0;
+	findArea(0, 0, arr[0][0], true, true);
+	printf("%d", _count);
+}
---
+++ 24.02.05/q12865.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int n, kg;
+int W[101];
+int V[101];
+int dp[101][100001]; // dp[n][k] n번째 제품까지 활용 가능, 무게가 k 일때 가치의 최대값
+// DP 인것은 확인했으나 접근 방법을 생각하지 못했다. 무게, 가치에 따라 dp를 세우는 것이 아닌
+// 얼마나 많은 물건들을 가지고 가치를 따질것인지 생각했어야 했다.
+int main(void)
+{
+	memset(dp, 0, sizeof(dp));
+	scanf("%d %d", &n, &kg);
+	for (int i = 1; i <= n; i++)
+	{
+		int _kg, _v;
+		scanf("%d %d", &_kg, &_v);
+		W[i] = _kg;
+		V[i] = _v;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j <= kg; j++)
+		{
+			if (j >= W[i])
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+			else
+				dp[i][j] = dp[i - 1][j];
+		}
+	}
+	printf("%d", dp[n][kg]);
+}
