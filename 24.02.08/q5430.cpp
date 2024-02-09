a458c4288e1585467a24ffaf7c53f07815076c17
bon jae ku <scottku@naver.com>
2024-02-09 14:04:18 +0900

    24.02.08 백준 문제풀이 5430 : reverse와 delete를 어찌 할 것이냐 << deque를 2개 활용해서 위치를 바꿔줌 천재다 천재 24.02.09 : 코드 약간 수정함

--- 24.02.08/q5430.cpp
+++ 24.02.08/q5430.cpp
@@ -1,78 +1,177 @@
-﻿#include <iostream>
-#include <vector>
-#include <algorithm>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
 #include <string>
-#include<deque>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n, t;
+int tc;
+vector<char> v_char;
 
-    cin >> t;
-    while (t--) {
-        string order, str;
-        bool reverse = false, error = false;
-        deque<int> dq;
-        cin >> order;
-        cin >> n;
-        cin >> str;
-        string s = "";
-        for (int i = 0; i < str.length(); i++) {
+int main(void)
+{
+	scanf("%d", &tc);
+	bool isFirst = true;
+	for (int _tc = 0; _tc < tc; _tc++)
+	{
+		v_char.clear();
+		deque<int> q_int;
+		deque<int> q_int_rev;
 
-            if (isdigit(str[i])) {
-                s += str[i];
+		while (true)
+		{
+			char c;
+			scanf("%c", &c);
+			if ((int)c == (int)'\n' || (int)c == (int)']')
+			{
+				if (v_char.empty())
+					continue;
+				else
+					break;
+			}
+			v_char.push_back(c);
+		}
 
-            }
-            else {
-                if (!s.empty()) {
-                    dq.push_back(stoi(s));
-                    s = "";
-                }
-            }
-        }
-        for (auto o : order) {
-            if (o == 'R') {
-                if (reverse)
-                    reverse = false;
-                else
-                    reverse = true;
-            }
-            else {
-                if (dq.empty()) {
-                    cout << "error" << '\n';
-                    error = true;
-                    break;
-                }
-                if (reverse)
-                    dq.pop_back();
-                else
-                    dq.pop_front();
-            }
-        }
-        if (!error) {
-            cout << '[';
-        }
-        if (reverse && !dq.empty()) {
-            for (auto o = dq.rbegin(); o != dq.rend(); o++) {
-                if (o == dq.rend() - 1)
-                    cout << *o;
-                else
-                    cout << *o << ',';
-            }
-        }
-        else if (!reverse && !dq.empty()) {
-            for (auto o = dq.begin(); o != dq.end(); o++) {
-                if (o == dq.end() - 1)
-                    cout << *o;
-                else
-                    cout << *o << ',';
-            }
-        }
-        if (!error)
-            cout << "]\n";
 
-    }
+
+		int num;
+		scanf("%d", &num);
+		int _count = 0;
+		if (num == 0)
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				char c;
+				scanf("%c", &c);
+			}
+		}
+		else
+		{
+			string str = "";
+			while (_count < num)
+			{
+				char temp;
+				scanf("%c", &temp);
+				if ((int)temp == (int)'[' || (int)temp == (int)']' || (int)temp == (int)',' || (int)temp == (int)'\n')
+				{
+					if ((int)temp == (int)',' || (int)temp == (int)']')
+					{
+						q_int.push_back(stoi(str));
+						_count++;
+						str = "";
+					}
+					continue;
+				}
+				else
+				{
+					str += temp;
+					//q_int.push_back((int)temp - 48);
+
+				}
+			}
+		}
+
+		int b_response = false;
+		int _reverse = 0;
+		for (int i = 0; i < v_char.size(); i++)
+		{
+			//if ((int)v_char[i] < 10)
+			//	continue;
+			if ((int)v_char[i] == (int)'R')
+			{	// 뒤집기
+				_reverse++;
+			}
+			else // 'D'
+			{
+				if (q_int.empty() && q_int_rev.empty())
+				{
+					b_response = true;
+					printf("%s\n", "error");
+					break;
+				}
+
+				if (_reverse % 2 == 1) // 반전 한 횟수가 홀수번이라면 뒤쪽에서 지우기
+					q_int.pop_back();
+				else
+					q_int.pop_front();
+			}
+
+			if (i == v_char.size() - 1) // 마지막이면 뒤집어줘야함
+			{
+				if (_reverse % 2 == 1) // 반전 한 횟수가 홀수번이라면
+				{
+					if (q_int.empty()) // q_int_rev가 가득 차있다.
+					{
+						while (!q_int_rev.empty())
+						{
+							q_int.push_back(q_int_rev.back());
+							q_int_rev.pop_back();
+						}
+					}
+					else
+					{
+						while (!q_int.empty())
+						{
+							q_int_rev.push_back(q_int.back());
+							q_int.pop_back();
+						}
+					}
+				}
+				_reverse = 0;
+			}
+		}
+
+		if (b_response) // 이미 대답이 된 상태면
+			continue;
+		else
+		{
+			if (q_int.empty() && q_int_rev.empty())
+			{
+				printf("%s\n", "[]");
+			}
+			else if (q_int.empty())
+			{
+				printf("%c", '[');
+				while (!q_int_rev.empty())
+				{
+					int t = q_int_rev.front();
+					q_int_rev.pop_front();
+					if (q_int_rev.empty())
+					{
+						printf("%d%c", t, ']');
+					}
+					else
+						printf("%d%c", t, ',');
+				}
+				printf("\n");
+			}
+			else if (q_int_rev.empty())
+			{
+				printf("%c", '[');
+				while (!q_int.empty())
+				{
+					int t = q_int.front();
+					q_int.pop_front();
+					if (q_int.empty())
+					{
+						printf("%d%c", t, ']');
+					}
+					else
+						printf("%d%c", t, ',');
+				}
+				printf("\n");
+			}
+		}
+	}
 }
