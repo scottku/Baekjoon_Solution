6556ec17548f2c8137de152433af2a7210b85d8d
bon jae ku <scottku@naver.com>
2024-02-11 21:12:22 +0900

    24.02.11 백준 문제풀이 q2293 : 처음에 BFS 느낌으로 동전을 하나씩 추가할까 했지만, 숫자가 꽤나 커서 시간 초과가 날것으로 생각함. 10의 가치를 만들기 위해서 9의 가치를 가지는 경우의 수에  1 동전을 1개 추가 + 8의 가치를 가지는 경우의 수에 2 동전을 추가 + 5의 가치를 가지는 경우의 수에 5 동전을 추가한다는 것을 파악해 dp로 변경해 풀이에 성공

---
+++ 24.02.11/q2293.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int n, k;
+int dp[10001]; // dp[a] = b : 가치 a를 갖는 경우의 수는 b 이다
+vector<int> v_coin;
+
+int main(void)
+{
+	memset(dp, 0, sizeof(dp));
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		v_coin.push_back(temp);
+	}
+	sort(v_coin.begin(), v_coin.end(), less<int>());
+	dp[0] = 1; //  0원을 만드는 것 = 아무것도 선택하지 않기 = 1가지
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = v_coin[i]; j <= k; j++)
+		{
+			dp[j] += dp[j - v_coin[i]];
+		}
+	}
+
+	printf("%d", dp[k]);
+}
