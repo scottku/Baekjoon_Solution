633103e2b7dee41efc7e553d8a2d90015c55e662
bon jae ku <scottku@naver.com>
2024-02-14 21:26:19 +0900

    24.02.14 백준 문제풀이 1717 : 처음엔 브루트포스 방식으로 진행, 하지만 최대 연산 횟수가 1,000,000 * 100,000 인 것을 확인 다음으로는 linked list로 부모와 자식들을 연결하는 방식을 고려. 구현 도중 그냥 배열에 부모를 지정해두면 되지 않나 싶어 해당 번호의 배열에 부모를 지정해두는 식으로 진행. 처음엔 집합에 포함되지 않는 녀석들의 부모를 없다는 의미로 0으로 정해뒀으나 숫자 0조차 해당 집합에 포함되는 수라 집합에 포함되지 않는 친구들은 본인을 부모로 지정하도록 설정. 무난하게 정답

---
+++ 24.02.14/q1717.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+int parent[1000001];
+
+int findParent(int _start)
+{
+	if (parent[_start] == _start)
+		return _start;
+	else
+		return findParent(parent[_start]);
+}
+
+void unionParent(int _a, int _b)
+{
+	int a = findParent(_a);
+	int b = findParent(_b);
+	if (a > b)
+		parent[a] = b;
+	else
+		parent[b] = a;
+}
+
+int main(void)
+{
+	memset(parent, 0, sizeof(parent));
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i <= n; i++)
+	{
+		parent[i] = i;
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int calc, a, b;
+		scanf("%d %d %d", &calc, &a, &b);
+		if (calc == 0) // 둘을 합쳐라
+		{
+			unionParent(a, b);
+		}
+		else // calc == 1 / 같은 곳에 포함되어있는가를 체크?
+		{
+			if (a == b)
+				printf("%s\n", "YES");
+			else
+			{
+				int a_parent = findParent(a);
+				int b_parent = findParent(b);
+				if (a_parent == b_parent) // 같은 부모 = 같은 집합
+					printf("%s\n", "YES");
+				else
+					printf("%s\n", "NO");
+			}
+		}
+	}
+}
