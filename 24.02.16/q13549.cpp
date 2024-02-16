276439380b543fbf176ccb9361ea8bb0ad4304e6
bon jae ku <scottku@naver.com>
2024-02-16 20:26:22 +0900

    24.02.16 백준 문제풀이 13549 : 우선순위 큐를 활용해야 했다. 횟수가 오르지 않는 조건이 있었기에 횟수가 오르지 않는 조건을 최대한 포함하는 녀석을 우선으로 잡았어야 했다.

---
+++ 24.02.16/q13549.cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >pq;
+bool visit[100001];
+
+int main(void)
+{
+	memset(visit, false, sizeof(visit));
+	scanf("%d %d", &n, &m);
+	pq.push({ 0, n });
+
+	int answer = 0;
+	while (!pq.empty())
+	{
+		int time = pq.top().first;
+		int num = pq.top().second;
+		pq.pop();
+
+		if (num == m)
+		{
+			printf("%d", time);
+			break;
+		}
+
+		visit[num] = true;
+
+		if (num + 1 <= 100000 && !visit[num + 1])
+		{
+			pq.push({ time + 1, num + 1 });
+		}
+		if (num - 1 >= 0 && !visit[num - 1])
+		{
+			pq.push({ time + 1, num - 1 });
+		}
+		if (num * 2 <= 100000 && !visit[num * 2])
+		{
+			pq.push({ time, num * 2 });
+		}
+	}
+}
