14fd6fa5bad2ccae49b2bc0326a930030817c5b9
bon jae ku <scottku@naver.com>
2024-02-17 22:28:39 +0900

    24.02.17 백준 문제풀이 2294 : 이 전의 우선순위 큐 방식과 마찬가지로 갯수가 가장 적은 친구가 답이 되는 것이므로 동전 갯수가 가장 적게되는 친구를 우선으로 선택해 진행했다.

---
+++ 24.02.17/q2294.cpp
@@ -0,0 +1,84 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >pq;
+bool visit[10001];
+bool _coin[100001];
+vector<int> v_coin;
+
+int main(void)
+{
+	memset(visit, false, sizeof(visit));
+	memset(_coin, false, sizeof(_coin));
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		if (!_coin[temp])
+		{
+			_coin[temp] = true;
+			v_coin.push_back(temp);
+		}
+	}
+	sort(v_coin.begin(), v_coin.end(), less<int>());
+	for (int i = 0; i < v_coin.size(); i++)
+	{
+		if (v_coin[i] == m)
+		{
+			printf("%d", 1);
+			return 0;
+		}
+		else if (v_coin[i] < m)
+		{
+			visit[v_coin[i]] = true;
+			pq.push({ 1, v_coin[i] });
+		}
+	}
+
+	while (!pq.empty())
+	{
+		int num = pq.top().first;
+		int value = pq.top().second;
+		pq.pop();
+
+		for (int i = 0; i < v_coin.size(); i++)
+		{
+			if (v_coin[i] + value == m)
+			{
+				printf("%d", num + 1);
+				return 0;
+			}
+			else if (v_coin[i] + value < m)
+			{
+				if (!visit[v_coin[i] + value])
+				{
+					visit[v_coin[i] + value] = true;
+					pq.push({ num + 1, v_coin[i] + value });
+				}
+			}
+			else
+			{
+
+			}
+		}
+	}
+	printf("%d", -1);
+}
