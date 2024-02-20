0f58abf128a4edafaa5fb837e66f28490462a8ff
bon jae ku <scottku@naver.com>
2024-02-20 21:53:55 +0900

    24.02.20 백준 문제풀이 2565 : 당연히 가장 많이 겹치는 전기줄부터 없애면 될 것이라 생각해 탐욕 알고리즘을 활용해 풀었으나 풀리지 않아 아예 접근하지 못했다. 앞 전기줄보다 뒷 전기줄이 왼쪽 전신주, 오른쪽 전신주 숫자가 높아야 한다는 것은 가장 긴 증가수열을 선택하는 것과 마찬가지라는 힌트를 확인하고 소름이 돋음. 정말 한번에 이러한 사실을 캐치해서 풀어내는 사람이 있을까? 살짝의 무력감이 밀려왔다.

---
+++ 24.02.20/q2565.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n;
+vector<pair<int, int>> v;
+int dp[101];
+
+int main(void)
+{
+	memset(dp, 0, sizeof(dp));
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		int _s, _e;
+		scanf("%d %d", &_s, &_e);
+		v.push_back({ _s, _e });
+	}
+	sort(v.begin(), v.end(), less<pair<int, int>>());
+	dp[1] = 1;
+	int _max = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = 1; // 자기 혼자만이라면 1개
+		for (int j = 1; j < i; j++)
+		{
+			if (v[i - 1].second > v[j - 1].second) // 오름차순이라면
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		_max = max(dp[i], _max);
+	} // n 번쨰일 때 최대 오름차순 배열 길이가 나옴
+	printf("%d", n - _max);
+}
