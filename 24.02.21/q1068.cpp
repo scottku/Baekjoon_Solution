214c4665da8bc88afedcc0caf13ca4c391edc148
bon jae ku <scottku@naver.com>
2024-02-21 22:22:19 +0900

    24.02.21 백준 문제풀이 2470 : 숫자가 워낙 커서 시간초과가 나지 않으려면 적어도 O(N) 정도는 가야할 것 같았는데 방법을 몰랐음. 투 포인터 알고리즘이라는 새 알고리즘에 대해 알게 됨. 1068 : 노드를 없애면 child가 전부 사라지는 것을 활용하기 위해 parent 에게 child 를 저장하도록 이진 트리를 작성했으나, 예시와 모든 테스트 케이스가 이진트리로 주어졌을 뿐 그 어디에도 이진 트리라는 말은 없었다. 사람들이 78%에서 모두 걸리는 이유가 이 때문이었고 범인인 나 조차 그 굴레에서 벗어날 수 없었다. 결국 힌트를 보고 파악해 마무리 한 문제

---
+++ 24.02.21/q1068.cpp
@@ -0,0 +1,83 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n, del;
+vector<int> child[50];
+int _count = 0;
+
+void insertNode(int _node, int _parent) // _node 의 부모가 _parent 이다
+{
+	if (child[_parent][0] == -1)
+		child[_parent][0] = _node;
+	else
+		child[_parent].push_back(_node);
+}
+
+void findLeafNode(int _parent)
+{
+	if (_parent == del)
+		return;
+
+	if (child[_parent][0] != -1) // 1명인지 2명인지는 몰라도 자식이 있다.
+	{
+		if (child[_parent].size() >= 2) // 자식이 2명 이상이다 * 그림 때문에 헷갈렸지만 이진 트리라고 어디에도 말해주지 않았다 *
+		{
+			for (int i = 0; i < child[_parent].size(); i++)
+			{
+				if (child[_parent][i] != del)
+					findLeafNode(child[_parent][i]);
+			}
+		}
+		else // 한명이다
+		{
+			if (child[_parent][0] != del)
+				findLeafNode(child[_parent][0]);
+			else
+				_count++;
+		}
+	}
+	else
+	{
+		// 자식 둘 중 앞 녀석 조차 존재하지 않는다 = 자식이 없다
+		_count++;
+	}
+}
+
+int main(void)
+{
+	for (int i = 0; i < 50; i++)
+	{
+		child[i].push_back(-1);
+	}
+
+	scanf("%d", &n);
+	int _root = -1;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		if (temp == -1) // * root가 꼭 0번째 노드는 아니다 *
+			_root = i;
+		else
+			insertNode(i, temp);
+	}
+	scanf("%d", &del); // del 과 del 의 자식들은 없다고 본다.
+	findLeafNode(_root);
+	printf("%d", _count);
+}
---
+++ 24.02.21/q2470.cpp
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n;
+vector<int> v_minus;
+vector<int> v_plus;
+vector<int> v;
+
+int main(void)
+{
+	scanf("%d", &n);
+	int n_minus = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end(), less<int>());
+	int _start = 0;
+	int _end = v.size() - 1;
+	pair<int, int> p_answer = { 1000000001, 1000000001 };
+	while (_start < _end)
+	{
+		if (abs(p_answer.first + p_answer.second) > abs(v[_start] + v[_end]))
+		{
+			p_answer.first = v[_start];
+			p_answer.second = v[_end];
+		}
+
+		if (v[_start] + v[_end] > 0)
+		{
+			_end--;
+		}
+		else if (v[_start] + v[_end] < 0)
+		{
+			_start++;
+		}
+		else // 합이 0
+		{
+			break;
+		}
+	}
+	printf("%d %d", p_answer.first, p_answer.second);
+
+}
