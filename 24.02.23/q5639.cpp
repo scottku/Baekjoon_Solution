701562faf70d92194373501bc3ebf42390e76554
bon jae ku <scottku@naver.com>
2024-02-23 23:04:16 +0900

    24.02.23 백준 문제풀이 5639 : 새로운 노드가 입력됐을 때 그 전 부분 노드들이 출력되어야 한다는 것에 뭔가 스택에서 괄호 짝 맞춰지듯이 짝이 생겼을 때 빼내야 하나 싶었으나, 수 크기를 비교해야하는 상황에 top만 확인 가능한 스택은 사용이 불가함을 깨달음. 결국 노드의 왼쪽은 무조건 본인보다 작음 + 오른쪽은 무조건 본인보다 크다 조건을 다시 확인하고 부분으로 나누어 노드 다음으로 나오는 부분에서 해당 노드보다 작은 부분 큰 부분이 나오는걸 확인해 끝까지 밀고들어가는 방식으로 풀이함

---
+++ 24.02.23/q5639.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int arr[10005];
+int i;
+
+void post(int start, int end) {
+	if (start >= end) return;
+
+	for (i = start + 1; i < end; i++)
+		if (arr[start] < arr[i]) break;
+
+	post(start + 1, i);
+	post(i, end);
+	printf("%d\n", arr[start]);
+
+	return;
+}
+
+int main() {
+	int n = 0, x;
+
+	while (scanf("%d", &x) != EOF)
+		arr[n++] = x;
+
+	post(0, n);
+
+	return 0;
+}
