6be129c94a5f3707288383e975e80cd88309eecd
bon jae ku <scottku@naver.com>
2024-02-27 23:29:42 +0900

    24.02.27 백준 문제풀이 1753 : - 가중치가 없어서 다익스트라 알고리즘을 활용, 다익스트라 알고리즘 특성상 시작하는 곳에서 가장 가중치가 낮은 곳을 우선해서 다음 노드로 넘어가기 때문에 우선순위 큐를 활용하면 배열을 활용할 때 보다 선형탐색으로 가장 낮은 복잡도를 찾는 것이 단축되어 시간 복잡도가 단순해진다고 함. 14500 : DFS와 브루트포스가 섞인 문제, 물론 모든 테트로미노 형태를 미리 지정해두고 해당 모양에 대한 반복문을 설정해도 괜찮지만 모양이 빠진다. ㅗ 모양 제외한 나머지는 DFS 방식으로 진행이 가능하므로 DFS 방식으로 최댓값을 찾고, ㅗ 모양은 중심점을 기준으로 주변 3 군데를 지정해 더하는 식으로 최댓값을 찾아냈다. 여담으로 아무 생각없이 반복분 내에 memset 함수를 넣어뒀는데, 해당 함수가 수만 수십만번 반복되다보니 엄청나게 시간이 오래걸렸다(아슬아슬하게 통과하는 수준). 이런 부분은 조심해야 할 것.

---
+++ 24.02.27/q14500.cpp
@@ -0,0 +1,103 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+// 시간 체크용
+//#include <time.h>
+// clock_t start, end;
+// start = clock();
+// 체크해보고싶은 함수
+// end = clock();
+// double result = end - start;
+// printf("%f", result / CLOCKS_PER_SEC);
+
+using namespace std;
+
+int n, m;
+int arr[501][501];
+bool visit[501][501];
+int answer = 0;
+int dx[4] = { -1, 1, 0, 0 }; // 상하좌우
+int dy[4] = { 0, 0, -1, 1 };
+
+void DFS(int x, int y, int _total, int _count) // _count = n 번째 칸이라는 뜻
+{
+	visit[x][y] = true;
+
+	if (_count == 4)
+	{
+		answer = max(answer, _total);
+		visit[x][y] = false; // 돌아갈 때 visit을 취소해야 다음에 다른 방식으로도 접근 가능
+		return;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		int _dx = x + dx[i];
+		int _dy = y + dy[i];
+		if (_dx > 0 && _dx <= n && _dy > 0 && _dy <= m && !visit[_dx][_dy])
+		{
+			DFS(_dx, _dy, _total + arr[_dx][_dy], _count + 1);
+		}
+	}
+	visit[x][y] = false; // 돌아갈 때 visit을 취소해야 다음에 다른 방식으로도 접근 가능
+}
+
+void CharA(int x, int y) // 중심점을 기준으로 주면 3방향을 선택해 ㅏ ㅗ 모양을 만듦
+{
+	int sum, _dx, _dy;
+	for (int i = 0; i < 4; i++)
+	{
+		sum = arr[x][y];
+		for (int dir = 0; dir < 4; dir++)
+		{
+			if (dir == i)
+				continue;
+			_dx = x + dx[dir];
+			_dy = y + dy[dir];
+			if (_dx > 0 && _dx <= n && _dy > 0 && _dy <= m)
+				sum += arr[_dx][_dy];
+		}
+		answer = max(answer, sum);
+	}
+}
+
+int main(void)
+{
+	memset(arr, 0, sizeof(arr));
+	memset(visit, false, sizeof(visit));
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			int temp;
+			scanf("%d", &temp);
+			arr[i][j] = temp;
+		}
+	}
+
+	for (int i = 1; i <= n; i++) // 가운데 부분에 튀어나온 ㅗ 모양은 DFS로 확인 불가능함
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			DFS(i, j, arr[i][j], 1);
+			CharA(i, j);
+		}
+	}
+
+	printf("%d", answer);
+}
---
+++ 24.02.27/q1753.cpp
@@ -0,0 +1,74 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+#define INF 30000000;
+
+int vertex, line;
+vector < pair<int, int>> v_arr[20001];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int arr[20001];
+
+int main(void)
+{
+	scanf("%d %d", &vertex, &line);
+	for (int i = 1; i <= vertex; i++)
+	{
+		arr[i] = INF;
+	}
+	int _start;
+	scanf("%d", &_start);
+	arr[_start] = 0;
+	for (int i = 0; i < line; i++)
+	{
+		int u, v, w;
+		scanf("%d %d %d", &u, &v, &w);
+		v_arr[u].push_back({ v,w }); // 점 u와 점 v가 연결되며 그 가중치는 w이다.
+	}
+
+	arr[_start] = 0;
+	pq.push({ 0, _start });
+	while (!pq.empty())
+	{
+		int v = pq.top().first;
+		int num = pq.top().second;
+		pq.pop();
+
+		for (int i = 0; i < v_arr[num].size(); i++)
+		{
+			int destination = v_arr[num][i].first;
+			int value = v_arr[num][i].second;
+
+			if (v + value < arr[destination])
+			{
+				arr[destination] = v + value;
+				pq.push({ v + value, destination });
+			}
+		}
+	}
+
+	for (int i = 1; i <= vertex; i++)
+	{
+		if (i == _start)
+			printf("%d\n", 0);
+		else if (arr[i] == 30000000) // 한번도 진입된 적 없음
+			printf("%s\n", "INF");
+		else
+			printf("%d\n", arr[i]);
+	}
+}
