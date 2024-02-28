1e36a14772644b2a076daae979c58c5a5a87113b
bon jae ku <scottku@naver.com>
2024-02-28 21:51:59 +0900

    24.02.28 백준 문제풀이 1987 : 한번 방문했던 곳도 다시 방문할 수 있도록 조정하는 DFS, visit을 관리하는 배열의 초기화를 해당 위치에서의 연산이 끝났을 때 바로바로 진행해줘야 하는 점이 기존 DFS 들과의 다른점. 11054 : 가장 긴 부분수열을 앞에서부터, 뒤에서부터 계산해서 서로 합한 값이 높은 것을 찾는 문제. 가장 긴 부분수열을 찾는 것을 확인했으니 그리 어렵지 않은 문제였다.

---
+++ 24.02.28/q11054.cpp
@@ -0,0 +1,78 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+// 시간 체크용
+//#include <time.h>
+// clock_t start, end;
+// start = clock();
+// 체크해보고싶은 함수
+// end = clock();
+// double result = end - start;
+// printf("%f", result / CLOCKS_PER_SEC);
+
+using namespace std;
+
+int n;
+int arr[1000];
+int dp_front[1000];
+int dp_back[1000];
+
+int main(void)
+{
+	memset(arr, 0, sizeof(arr));
+	memset(dp_front, 0, sizeof(dp_front));
+	memset(dp_back, 0, sizeof(dp_back));
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		arr[i] = temp;
+	}
+
+	// 앞에서부터, 뒤쪽에서부터 점점 증가하는 부분수열의 최대 길이 확인
+	dp_front[0] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] > arr[j])
+				dp_front[i] = max(dp_front[i], dp_front[j] + 1);
+		}
+		if (dp_front[i] == 0) // 앞에 수들 중 제일 작은 수랑 값이 같다
+			dp_front[i] = 1;
+	}
+	dp_back[n - 1] = 1;
+	for (int i = n - 2; i >= 0; i--)
+	{
+		for (int j = n - 1; j > i; j--)
+		{
+			if (arr[i] > arr[j])
+				dp_back[i] = max(dp_back[i], dp_back[j] + 1);
+		}
+		if (dp_back[i] == 0) // 뒤에 수들 중 제일 작은 수랑 값이 같다
+			dp_back[i] = 1;
+	}
+
+	int answer = 0;
+	for (int i = 0; i < n; i++)
+	{
+		answer = max(answer, dp_front[i] + dp_back[i] - 1);
+	}
+	printf("%d", answer);
+}
---
+++ 24.02.28/q1987.cpp
@@ -0,0 +1,91 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+// 시간 체크용
+//#include <time.h>
+// clock_t start, end;
+// start = clock();
+// 체크해보고싶은 함수
+// end = clock();
+// double result = end - start;
+// printf("%f", result / CLOCKS_PER_SEC);
+
+using namespace std;
+#define ARR_SIZE  21
+int R, C;
+char arr[ARR_SIZE][ARR_SIZE];
+int answer = 0;
+bool visit[26]; // 알파벳 갯수는 26개, 'A' => 65
+int dx[4] = { -1, 1, 0 ,0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+void DFS(int x, int y, int _count)
+{
+	int n_alphabet = (int)arr[x][y];
+	visit[n_alphabet - (int)'A'] = true;
+
+	if (_count > answer)
+		answer = _count;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int _dx = x + dx[i];
+		int _dy = y + dy[i];
+		if (_dx > 0 && _dx <= R && _dy > 0 && _dy <= C)
+		{
+			if (!visit[(int)arr[_dx][_dy] - (int)'A']) // 한번도 방문하지 않은 알파벳
+				DFS(_dx, _dy, _count + 1);
+		}
+	}
+
+	visit[n_alphabet - (int)'A'] = false; // 뒤로 돌아갈때는 기존에 방문했던 알파벳을 방문하지 않은 것으로 되돌려놔야 함.
+}
+
+int main(void)
+{
+	for (int i = 0; i < ARR_SIZE; i++)
+	{
+		for (int j = 0; j < ARR_SIZE; j++)
+		{
+			arr[i][j] = ' ';
+		}
+	}
+	memset(visit, false, sizeof(visit));
+
+	scanf("%d %d", &R, &C);
+	for (int i = 1; i <= R; i++)
+	{
+		for (int j = 1; j <= C; j++)
+		{
+			char c;
+			scanf("%c", &c);
+			if ((int)c == (int)'\n' || (int)c == (int)' ')
+			{
+				j--;
+				continue;
+			}
+			else
+			{
+				arr[i][j] = c;
+			}
+		}
+	}
+
+	DFS(1, 1, 1); // (1,1) 위치에서 시작, 현재 카운트는 1;
+
+	printf("%d", answer);
+}
