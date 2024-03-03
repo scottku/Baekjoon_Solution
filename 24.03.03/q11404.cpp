b5e93f285735f65075be3f8e3420e64fe8af54b2
bon jae ku <scottku@naver.com>
2024-03-03 21:41:59 +0900

    24.03.03 백준 문제풀이 11404 : 플로이드 워셜 문제, 길찾기 문제 중 모든 간선간의 연결을 확인할 수 있는 알고리즘 문제.

---
+++ 24.03.03/q11404.cpp
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int city, bus;
+int arr[101][101];
+#define INF 987654321
+
+int main()
+{
+	scanf("%d", &city);
+	scanf("%d", &bus);
+
+	for (int i = 1; i <= city; i++)
+	{
+		for (int j = 1; j <= city; j++)
+		{
+			arr[i][j] = INF;
+		}
+	}
+	for (int i = 1; i <= city; i++)
+		arr[i][i] = 0;
+
+
+	for (int i = 0; i < bus; i++)
+	{
+		int from, to, value;
+		scanf("%d %d %d", &from, &to, &value);
+		if (value < arr[from][to])
+			arr[from][to] = value;
+	}
+
+	for (int k = 1; k <= city; k++)
+	{
+		for (int i = 1; i <= city; i++)
+		{
+			for (int j = 1; j <= city; j++)
+			{
+				arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+			}
+		}
+	}
+
+	for (int i = 1; i <= city; i++)
+	{
+		for (int j = 1; j <= city; j++)
+		{
+			if (arr[i][j] == INF)
+				printf("0 ");
+			else
+				printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
