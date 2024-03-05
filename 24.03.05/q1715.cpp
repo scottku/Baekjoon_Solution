203150710114b17508d9f6ba73798137bebc4936
bon jae ku <scottku@naver.com>
2024-03-05 19:35:06 +0900

    24.03.05 백준 문제풀이 2110 : 입력 범위가, 갯수가 너무 많아서 무조건 이분탐색일 것으로 예상, 결국 구하고자 하는 것은 가장 긴 공유기 거리였으므로 공유기 거리를 이분탐색으로 체크함 1715 : 결국 가장 작은 수 2개씩 더해가야 최종 값이 제일 적어지므로 우선순위 큐를 활용해 작은 수부터 더함

---
+++ 24.03.05/q1715.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+int n;
+
+int main(void)
+{
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		pq.push(temp);
+	}
+
+	int answer = 0;
+	while (pq.size() > 1) // size == 1 << 이때가 정답
+	{
+		int a = pq.top();
+		pq.pop();
+		int b = pq.top();
+		pq.pop();
+		answer += (a + b);
+		pq.push(a + b);
+	}
+
+	printf("%d", answer);
+}
---
+++ 24.03.05/q2110.cpp
@@ -0,0 +1,69 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+// 입력되는 값의 갯수와 범위가 어마어마하다
+// = 일반적인 시간 내에는 풀기 어렵다
+// = 시간복잡도가 적은 이진탐색일 확률이 매우 높다
+// = 어떤 것을 절반씩 줄여나가면서 확인할 것인가
+// = 결국 우리가 찾고자 하는 것은 공유기 간 거리
+// = 공유기 설치가 가능한 거리를 이진탐색으로 찾아보자
+
+vector<int> v;
+int N, C;
+
+int main(void)
+{
+	scanf("%d %d", &N, &C);
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		scanf("%d", &temp);
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end(), less<int>());
+
+	int _start = 1;
+	int _end = v[v.size() - 1];
+	int answer = 0;
+	while (_start <= _end)
+	{
+		int mid = (_start + _end) / 2;
+		int _count = 1; // 맨 앞에는 무조건 하나 박혀있음
+		int _before = v[0]; // 바로 전에 공유기가 설치된 집 위치
+		for (int i = 1; i < v.size(); i++)
+		{
+			if (_before + mid <= v[i])
+			{
+				_before = v[i];
+				_count++;
+			}
+		}
+		if (_count < C) // 갯수가 적다 = 텀이 짧아야 한다.
+			_end = mid - 1;
+		else
+		{
+			answer = mid; // _count == C
+			_start = mid + 1; // _count > C
+		}
+
+	}
+
+	printf("%d", answer);
+}
