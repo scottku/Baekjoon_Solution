f48b949532d2c5db469582af8df2ef505156e843
bon jae ku <scottku@naver.com>
2024-03-07 23:27:22 +0900

    2024.03.07 백준 문제풀이 1707 : 한 정점을 a라 하면 붙어있는 옆 정점들을 전부 b로 만드는 과정을 반복했을 때, a와 b가 겹치는 일 없이 나눠지는가를 확인하는 문제 1504 : 다익스트라를 활용하는 문제, 반드시 거쳐야하는 지점에 대해 너무 심각하게 받아들였음. 어차피 어느 방향으로 진행하던 결국 그 지점까지의 최단거리를 알려주는 것이기 때문에 크게 고민할 이유가 없었음 -> 고찰 : https://www.acmicpc.net/board/view/138176#post

---
+++ 24.03.07/q1504.cpp
@@ -0,0 +1,146 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+int _vertex, _edge;
+#define INF 987654321
+class myClass
+{
+public:
+	bool toV1;
+	bool toV2;
+	bool toN;
+	int dist;
+	int pos;
+	myClass(int _dist, int _pos, bool _v1, bool _v2, bool _n)
+	{
+		dist = _dist;
+		pos = _pos;
+		toV1 = _v1;
+		toV2 = _v2;
+		toN = _n;
+	}
+};
+int arr[801][801];
+int mustV1, mustV2;
+int arr1[801];
+int arr2[801];
+
+class comp
+{
+public:
+	bool operator()(const myClass& left, const myClass& right)
+	{
+		return left.dist > right.dist;
+	}
+};
+
+void findMinDist(int _start);
+
+int main(void)
+{
+	for (int i = 0; i < 801; i++)
+	{
+		for (int j = 0; j < 801; j++)
+		{
+			if (i == j)
+				arr[i][j] = 0;
+			else
+				arr[i][j] = INF;
+		}
+	}
+
+	scanf("%d %d", &_vertex, &_edge);
+	for (int i = 0; i < _edge; i++)
+	{
+		int a, b, c;
+		scanf("%d %d %d", &a, &b, &c);
+		if (arr[a][b] > c)
+			arr[a][b] = c;
+		if (arr[b][a] > c)
+			arr[b][a] = c;
+	}
+	scanf("%d %d", &mustV1, &mustV2);
+
+	findMinDist(mustV1);
+	findMinDist(mustV2);
+
+	int answer1 = -1;
+	int answer2 = -1;
+	if (arr1[1] < INF && arr1[mustV2] < INF && arr2[_vertex] < INF)
+		answer1 = arr1[1] + arr1[mustV2] + arr2[_vertex];
+	if (arr2[1] < INF && arr2[mustV1] < INF && arr1[_vertex] < INF)
+		answer2 = arr2[1] + arr2[mustV1] + arr1[_vertex];
+	if (answer1 == -1 && answer2 == -1)
+		printf("%d", -1);
+	else if (answer1 == -1)
+		printf("%d", answer2);
+	else if (answer2 == -1)
+		printf("%d", answer1);
+	else
+		printf("%d", min(answer1, answer2));
+}
+
+void findMinDist(int _start)
+{
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+	if (mustV1 == _start)
+	{
+		for (int i = 0; i < 801; i++)
+			arr1[i] = INF;
+		pq.push({ 0, _start });
+		while (!pq.empty())
+		{
+			int dist = pq.top().first;
+			int pos = pq.top().second;
+			pq.pop();
+
+			for (int i = 1; i <= _vertex; i++)
+			{
+				if (dist + arr[pos][i] < arr1[i]) // 연결되어있거나 더 작은 거리 값을 가지고 있다면
+				{
+					arr1[i] = dist + arr[pos][i];
+					pq.push({ arr1[i], i });
+				}
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 801; i++)
+			arr2[i] = INF;
+		pq.push({ 0, _start });
+		while (!pq.empty())
+		{
+			int dist = pq.top().first;
+			int pos = pq.top().second;
+			pq.pop();
+
+			for (int i = 1; i <= _vertex; i++)
+			{
+				if (dist + arr[pos][i] < arr2[i]) // 연결되어있거나 더 작은 거리 값을 가지고 있다면
+				{
+					arr2[i] = dist + arr[pos][i];
+					pq.push({ arr2[i], i });
+				}
+			}
+		}
+	}
+}
---
+++ 24.03.07/q1707.cpp
@@ -0,0 +1,95 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+#define _BLACK_ 1
+#define _WHITE_ 2
+
+vector<vector<int>> v;
+int test_case;
+int _vertex, _edge;
+int visit[20001];
+
+bool duplication(int vertex, int color);
+
+int main(void)
+{
+	scanf("%d", &test_case);
+	for (int i = 0; i < test_case; i++)
+	{
+		memset(visit, 0, sizeof(visit));
+		v.clear();
+
+		scanf("%d %d", &_vertex, &_edge);
+		for (int j = 0; j <= _vertex; j++)
+		{
+			v.push_back(vector<int>());
+		}
+		for (int j = 0; j < _edge; j++)
+		{
+			int a, b;
+			scanf("%d %d", &a, &b);
+			v[a].push_back(b);
+			v[b].push_back(a);
+		}
+
+		bool b_dup = false;
+		for (int j = 1; j <= _vertex; j++)
+		{
+			if (visit[j] == 0) // 한번도 방문되지 않은 곳
+			{
+				b_dup = duplication(j, _WHITE_);
+				if (b_dup)
+					break;
+			}
+		}
+		if (b_dup)
+			printf("%s\n", "NO");
+		else
+			printf("%s\n", "YES");
+	}
+}
+
+bool duplication(int vertex, int color)
+{
+	if (visit[vertex] != 0) // 방문된 곳
+	{
+		if (visit[vertex] == color) // 방문된 곳인데 같은 색으로 칠해짐 -> 루프 모양이어도 가능한 이유
+			return false;
+
+		return true;
+	}
+
+	visit[vertex] = color;
+	// 해당 정점과 연결된 다른 친구들은 다른 색으로 변경한다.
+	int next_color;
+	if (color == _WHITE_)
+		next_color = _BLACK_;
+	else
+		next_color = _WHITE_;
+
+	vector<int> tv = v[vertex];
+	for (int i = 0; i < tv.size(); i++)
+	{
+		if (duplication(tv[i], next_color))
+			return true;
+	}
+
+	return false;
+}
