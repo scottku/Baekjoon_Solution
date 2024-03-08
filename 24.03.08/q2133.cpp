e7f73acec411d7d82c51f0803f444deebb401ada
bon jae ku <scottku@naver.com>
2024-03-08 13:33:41 +0900

    24.03.08 백준 문제풀이 2133 : 높이가 3이라 한쪽은 반드시 2 x 1 타일로 채워져야 해서 2 x n 타일과 관련이 있을거라 처음에 추측. 하지만 2칸씩 나누다보니 열의 갯수가 많아지면 많아질수록 경우의 수가 너무 다양해짐 (위쪽에 가로 타일을 놓느냐 아래쪽에 가로 타일을 놓느냐 2 ^ n 의 경우) 따라서 올바른 방향이 아닐 것으로 생각. 결국 큰 범위를 작은 범위 둘로 나누어 DP 방식으로 풀어가는 것은 맞지만, 일부 배열부터는 전부 중복되는 방식이라 특이 케이스인 양 끝에 세로 타일 1개씩이 배치된 2가지 케이스를 제외하면 전부 중복. 따라서 처음 배치때는 모든 케이스를 확인하고 이후부터는 특이 케이스들만 고려해 경우의 수를 측정

---
+++ 24.03.08/q2133.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+int n;
+long long dp[31];
+
+int main(void)
+{
+	for (int i = 0; i < 31; i++)
+	{
+		dp[i] = 0;
+	}
+	scanf("%d", &n);
+	dp[2] = 3;
+	dp[4] = dp[2] * dp[2] + 2;  // 특이 케이스 2가지 발생 -> 양쪽에 세로 상자 1개씩이 세워져 있는 
+								// 케이스 
+	for (int i = 6; i <= n; i += 2)
+	{
+		dp[i] = dp[i - 2] * dp[2];
+		for (int j = i - 4; j >= 2; j -= 2)
+		{
+			dp[i] += dp[j] * 2; // 특이 케이스인 모양을 끼워넣는게 아니면 기존과 모양이 죄다 중복됨
+		}
+		dp[i] += 2; // 나누는거 없이 온전히 i 칸으로 타일을 채웠을 때 생기는 특이 케이스 2개
+	}
+	printf("%lld", dp[n]);
+}
