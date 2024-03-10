3b467393fe5c4dc65d3a5211a7823c909443eba4
bon jae ku <scottku@naver.com>
2024-03-10 21:16:19 +0900

    24.03.10 백준 문제풀이 1922 : 최단경로를 찾는데 특정위치에서의 최단경로가 아닌 모든 경로가 연결될 때 경로의 합을 구하는 문제, 어차피 최단경로를 구하는 방식은 다익스트라로 하면 될것 같아 시작은 다익스트라로, 최단경로를 구하는 방식은  더해진 값이 아닌 그 경로 각자의 값을 그대로 저장해둠으로써 나중에 더해주면 정답.

---
+++ 24.03.10/q1922.cpp
@@ -0,0 +1,86 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n, m;
+int arr[1001][1001];
+#define INF 987654321
+bool visit[1001];
+int value[1001];
+
+int main()
+{
+    memset(visit, false, sizeof(visit));
+    for (int i = 0; i < 1001; i++)
+    {
+        if (i == 1)
+            value[i] = 0;
+        else
+            value[i] = INF;
+        for (int j = 0; j < 1001; j++)
+        {
+            arr[i][j] = INF;
+        }
+    }
+    scanf("%d", &n);
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, v;
+        scanf("%d %d %d", &a, &b, &v);
+        arr[a][b] = v;
+        arr[b][a] = v;
+    }
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({ 0, 1 });
+    while (!pq.empty())
+    {
+        int _v = pq.top().first;
+        int _start = pq.top().second;
+        pq.pop();
+        if (visit[_start] || value[_start] != _v)
+            continue;
+
+        visit[_start] = true;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == _start)
+                continue;
+
+            if (arr[_start][i] != INF)
+            {
+                if (visit[i])
+                    continue;
+
+                if (value[i] > arr[_start][i])
+                {
+                    value[i] = arr[_start][i];
+                    pq.push({ value[i], i });
+                }
+            }
+        }
+    }
+
+    int answer = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (value[i] != INF)
+            answer += value[i];
+    }
+    printf("%d", answer);
+}
