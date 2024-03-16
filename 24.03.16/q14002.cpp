31712d501c241d6eb596379942ad531e7c139367
bon jae ku <scottku@naver.com>
2024-03-16 23:14:05 +0900

    24.03.16 백준 문제풀이 14002 : 가장 긴 증가하는 수열 찾는 DP 문제, 이 문제에선 배열 그 자체까지 나타내야 했기에 해당 배열을 나타낼 벡터 배열을 하나 더 추가해서 진행했다. 참고로 이 문제풀이를 꾸준이 하기 직전에 약 3시간짜리 코딩테스트 문제풀이를 훑어봤었는데, 그때 해당 문제를 풀려다가 못풀었었다. 근데 지금와서는 뚝딱 풀어내는거 보니 그래도 실력이 아예 제자리 걸음은 아닌듯 ㅋㅋ

---
+++ 24.03.16/q14002.cpp
@@ -0,0 +1,69 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int n;
+int arr[1001];
+int dp[1001];
+vector<int> v_dp[1001];
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        int temp;
+        scanf("%d", &temp);
+        arr[i] = temp;
+        dp[i] = 1;
+        v_dp[i] = vector<int>();
+        v_dp[i].push_back(temp);
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = i - 1; j >= 1; j--)
+        {
+            if (arr[i] > arr[j])
+            {
+                if (dp[i] < dp[j] + 1)
+                {
+                    dp[i] = dp[j] + 1;
+                    v_dp[i] = v_dp[j];
+                    v_dp[i].push_back(arr[i]);
+                }
+            }
+        }
+    }
+
+    int _max = 0;
+    int _max_pos = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (dp[i] > _max)
+        {
+            _max = dp[i];
+            _max_pos = i;
+        }
+    }
+    printf("%d\n", dp[_max_pos]);
+    for (int i = 0; i < v_dp[_max_pos].size(); i++)
+    {
+        printf("%d ", v_dp[_max_pos][i]);
+    }
+}
