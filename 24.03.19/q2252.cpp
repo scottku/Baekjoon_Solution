f614c43f6c7f596a4439c7e06414f1eb89386589
bon jae ku <scottku@naver.com>
2024-03-19 20:57:04 +0900

    24.03.19 백준 문제풀이 2252 : 특정 조건을 만족하게 됐을 때 해당 작업을 진행하는 문제, "위상 정렬" 이라는 알고리즘이 새로이 나타남. 기본적으로는 아무 조건이 없는 녀석들 부터 처리 후 다른 녀석들의 조건이 만족되게 되면 해당 녀석들을 큐에 넣어 순차적으로 처리하는 방식이지만, 특정 녀석의 조건을 만족시키기 위해 해당 조건에 포함되는 녀석을 찾아 들어가서 일단 모든 조건을 만족시키는 DFS 방식으로도 문제를 풀 수 있었다.

---
+++ 24.03.19/q2252.cpp
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+int n, m;
+vector<int> arr[32001];
+bool visit[32001];
+
+class comp
+{
+public:
+	bool operator() (const pair<int, int>& num1, const pair<int, int>& num2)
+	{
+		return num1.first > num2.first;
+	}
+};
+
+void printParent(int num);
+
+int main(void)
+{
+	memset(visit, false, sizeof(visit));
+	for (int i = 1; i <= 32000; i++)
+	{
+		arr[i] = vector<int>();
+	}
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < m; i++)
+	{
+		int f, b;
+		scanf("%d %d", &f, &b);
+		arr[b].push_back(f); // b 앞에 f 라는 녀석이 위치해야 한다.
+	}
+	priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
+	for (int i = 1; i <= n; i++)
+	{
+		pq.push({ arr[i].size(), i });
+	}
+	while (!pq.empty())
+	{
+		int size = pq.top().first;
+		int num = pq.top().second;
+		pq.pop();
+
+		if (!visit[num]) // 본인이 이미 출력되지 않았다면
+			printParent(num);
+	}
+}
+
+void printParent(int num)
+{
+	if (arr[num].size() != 0)
+	{
+		for (int i = 0; i < arr[num].size(); i++)
+		{
+			if (!visit[arr[num][i]]) // 나보다 앞에 있어야 할 녀석이 아직 출력이 안됐다면
+				printParent(arr[num][i]); // 그 녀석을 출력시키기 위해 DFS
+		}
+	}
+
+	visit[num] = true;
+	printf("%d ", num);
+}
