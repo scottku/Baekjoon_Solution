ac0c90be071756e47294103edbbe6f9ca93913fb
bon jae ku <scottku@naver.com>
2024-04-03 20:57:19 +0900

    24.04.03 백준 문제풀이 15684 : 단순 브루트포스 문제, 특정 위치에 배치 후 조건을 만족시키지 못하면 바로바로 뒤로 가서(백트래킹) 다음 조건을 파악(dfs), 배치 수가 3 이상이면 계산이 더이상 진행되지 않으므로 계산 량 자체는 많지 않아 브루트 포스로도 큰 문제가 없음

---
+++ 24.04.03/q15684.cpp
@@ -0,0 +1,96 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N, M, H;
+bool arr[31][11]; // a번째 라인에 가로선, b -> b+1을 잇는다
+int answer = 987654321;
+
+bool endGame()
+{
+    for (int i = 1; i <= N; i++)
+    {
+        int col = i;
+        for (int j = 1; j <= H; j++)
+        {
+            if (arr[j][col])
+                col++;
+            else if (arr[j][col - 1])
+                col--;
+            else
+            {
+
+            }
+        }
+        if (col != i)
+            return false;
+    }
+    return true;
+}
+
+void dfs(int n_line, int cnt)
+{
+    if (n_line == cnt)
+    {
+        if (endGame())
+        {
+            cout << n_line;
+            exit(0);
+        }
+        return;
+    }
+
+    for (int i = 1; i < N; ++i)
+    {
+        for (int j = 1; j <= H; ++j)
+        {
+            if (arr[j][i] || arr[j][i - 1] || arr[j][i + 1])
+                continue; // 선 못그림
+            arr[j][i] = true;
+            dfs(n_line, cnt + 1);
+            arr[j][i] = false; // 임시로 그린 선 리셋
+
+            while (!arr[j][i - 1] && !arr[j][i + 1])
+                j++;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    memset(arr, false, sizeof(arr));
+
+    cin >> N >> M >> H;
+    for (int i = 0; i < M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        arr[a][b] = true;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        dfs(i, 0);
+    }
+
+    cout << -1;
+}
