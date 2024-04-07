8a636f633cf88b6c5a140dd1884eeda1942d9014
bon jae ku <scottku@naver.com>
2024-04-07 23:27:52 +0900

    24.04.07 백준 문제풀이 9461 : 무난한 DP 문제, 오랜만에 풀어보는 쉬운 문제

---
+++ 24.04.07/q9461.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int tc;
+int N;
+long long dp[101];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> tc;
+    for (int test_case = 0; test_case < tc; test_case++)
+    {
+        cin >> N;
+
+        dp[1] = dp[2] = dp[3] = 1;
+        dp[4] = dp[5] = 2;
+        for (int i = 6; i <= N; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+
+        cout << dp[N] << '\n';
+    }
+}
