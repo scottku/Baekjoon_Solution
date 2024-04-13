b85469a8afb50d46dec7975d57465e4516cf504b
bon jae ku <scottku@naver.com>
2024-04-13 21:28:23 +0900

    24.04.13 백준 문제풀이 2193 : DP 문제, 뒷자리가 0이나 1이냐에 따라 다르게 저장해서 풀어야 풀리며, 숫자가 커지면 int 범위를 넘어가기 때문에 처음부터 long long으로 지정했어야 함

---
+++ 24.04.13/q2193.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N;
+long long dp[91][2];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= 90; i++)
+    {
+        dp[i][0] = 0;
+        dp[i][1] = 0;
+    }
+
+    dp[1][0] = 0; // 1
+    dp[1][1] = 1;
+    dp[2][0] = 1;
+    dp[2][1] = 0;
+
+    for (int i = 3; i <= N; i++)
+    {
+        dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+        dp[i][1] = dp[i - 1][0];
+    }
+    cout << dp[N][0] + dp[N][1];
+}
