b36c7cb1eaf7eb978ea41333d5898679638da9b5
bon jae ku <scottku@naver.com>
2024-04-15 20:48:11 +0900

    24.04.15 백준 문제풀이 7579 : '배낭 풀이' 라는 카테고리에 포함되어있는 문제, 제한조건에 따라 최상의 조건을 찾는 문제인데, 보통 특정 무게 이하, 특정 가치 이상에 따른 최대 가치, 최소 무게 같은 방식으로 진행되며, 모든 갯수 N개를 전부 둘러봤을 때, 무게 i에 대해 얻을 수 있는 최대 가치 = max(dp[N][i], dp[N-1][i - cost] + weight) 같은 방식으로 진행된다.

---
+++ 24.04.15/q7579.cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+int N, M, ans, sum;
+int bite[101], cost[101];
+int dp[101][10001];
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) cin >> bite[i];
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> cost[i];
+		// sum : 모든 비용들의 합
+		sum += cost[i];
+	}
+
+	// dp[i][j] == i번째 앱까지 탐색했을 때 
+	// j비용을 소모해서 얻을 수 있는 최대 메모리
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j <= sum; j++)
+		{
+			if (j - cost[i] >= 0)
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - cost[i]] + bite[i]);
+
+			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+		}
+	}
+
+	for (int i = 0; i <= sum; i++)
+	{
+		if (dp[N][i] >= M)
+		{
+			cout << i << endl;
+			break;
+		}
+	}
+
+
+}
