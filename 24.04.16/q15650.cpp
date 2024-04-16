e916807a4a9387b4203cde5b29557e1b956fd4fb
bon jae ku <scottku@naver.com>
2024-04-16 20:38:00 +0900

    24.04.16 백준 문제풀이 15650 : 조합론 문제, DFS를 활용해서 갯수에 따른 조합을 체크한다.

---
+++ 24.04.16/q15650.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+#define MAX 9
+
+int n, m;
+int arr[MAX] = { 0, };
+bool visited[MAX] = { 0, };
+
+void dfs(int num, int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = num; i <= n; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = true;
+            arr[cnt] = i;
+            dfs(i + 1, cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    dfs(1, 0);
+}
