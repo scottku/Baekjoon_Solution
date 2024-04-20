356ee04139bcf9c9b666193e122073495ef60e3b
bon jae ku <scottku@naver.com>
2024-04-20 22:00:45 +0900

    24.04.20 백준 문제풀이 15652 : 조합론 문제, DFS를 활용

---
+++ 24.04.20/q15652.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void DFS(int index, int count)
+{
+	if (count == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+	}
+	else
+	{
+		for (int i = index; i <= N; i++)
+		{
+			arr[count] = i;
+			DFS(i, count + 1);
+			arr[count] = 0;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++)
+		arr[i] = 0;
+
+	DFS(1, 0);
+}
