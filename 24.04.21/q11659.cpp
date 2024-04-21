5cf4e4f952c4c90fb7d7a6f28929695752885d0f
bon jae ku <scottku@naver.com>
2024-04-21 21:23:30 +0900

    24.04.21 백준 문제풀이 11659 : 누적합 문제, 누적합은 dp를 활용해 원하는 구간을 얻기 위해 두 dp 값을 빼버리면 간단하게 확인할 수 있다.

---
+++ 24.04.21/q11659.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N, M;
+int arr[100001];
+int dp[100001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	int temp;
+	dp[0] = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> temp;
+		arr[i] = temp;
+		dp[i] = dp[i - 1] + temp;
+	}
+
+	int from, to;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> from >> to;
+		cout << dp[to] - dp[from - 1] << '\n';
+	}
+}
