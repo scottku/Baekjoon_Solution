d59bab2de3bdbd50dbf0c9fdf4f33cfbdf93ca58
bon jae ku <scottku@naver.com>
2024-04-23 22:28:03 +0900

    24.04.23 백준 문제풀이 2473 : 왜 굳이 3개의 용액을 고른다고 한건지 의심하게 만든 문제, 숫자가 커 long long 형식을 사용해야 했고, 왠만한 접근법으로는 싹다 시간초과가 나게 생긴 상황, 특정 상황에 맞춰나가는 선택을 연속적으로 행해야 했기 때문에 점 하나를 고정해두고 투포인터를 활용해 가장 0에 가까워지는 1+2개의 인자를 뽑아낼 수 있었다.

---
+++ 24.04.23/q2473.cpp
@@ -0,0 +1,123 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+int N;
+vector<long long> v;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	long long temp;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end(), less<int>());
+	vector<long long> v_ans = { LLONG_MAX, LLONG_MAX, LLONG_MAX };
+	vector<long long> v_temp = { 0, 0, 0 };
+	long long answer = LLONG_MAX;
+	for (int i = 0; i < N; i++)
+	{
+		int front = i + 1;
+		int back = N - 1;
+		if (front >= back)
+			break;
+
+		long long temp;
+		long long temp_before = LLONG_MAX;
+		while (true)
+		{
+			if (front >= back)
+				break;
+			temp = v[i] + v[front] + v[back];
+			if (temp_before == LLONG_MAX)
+			{
+				v_temp[0] = v[i];
+				v_temp[1] = v[front];
+				v_temp[2] = v[back];
+				if (temp < 0)
+				{
+					front++;
+					temp_before = temp;
+				}
+				else if (temp == 0)
+				{
+					cout << v[i] << ' ' << v[front] << ' ' << v[back];
+					exit(0);
+					return 0;
+				}
+				else
+				{
+					back--;
+					temp_before = temp;
+				}
+			}
+			else
+			{
+				if (abs(temp) < abs(temp_before)) // 절댓값이 더 작다 = 0에 가깝다 = 올바른 방향으로 이동 중
+				{
+					v_temp[0] = v[i];
+					v_temp[1] = v[front];
+					v_temp[2] = v[back];
+					if (temp > 0)
+					{
+						back--;
+						temp_before = temp;
+					}
+					else if (temp == 0)
+					{
+						cout << v[i] << ' ' << v[front] << ' ' << v[back];
+						exit(0);
+						return 0;
+					}
+					else
+					{
+						front++;
+						temp_before = temp;
+					}
+				}
+				else // 숫자가 모두 다르기 때문에 전 계산값하고 값이 같을 수는 없다
+				{
+					// 절댓값이 더 커졌다?
+					if (temp > 0)
+						back--;
+					else
+						front++;
+				}
+			}
+		}
+
+		if (abs(answer) > abs(temp_before))
+		{
+			answer = temp_before;
+			v_ans[0] = v_temp[0];
+			v_ans[1] = v_temp[1];
+			v_ans[2] = v_temp[2];
+		}
+	}
+
+	cout << v_ans[0] << ' ' << v_ans[1] << ' ' << v_ans[2];
+}
