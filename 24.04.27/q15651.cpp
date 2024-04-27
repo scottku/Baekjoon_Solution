db9a44b4c69aca9cdccfccb235de137f70448d4f
bon jae ku <scottku@naver.com>
2024-04-27 21:03:42 +0900

    24.04.27 백준 문제풀이 15651 : DFS 문제, 조합 구하는 방식으로 순서대로 진행해주면 풀리는 간단한 문제

---
+++ 24.04.27/q15651.cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N, M;
+vector<vector<int>> v;
+vector<int> temp_v;
+
+void DFS(int count)
+{
+	if (count == M)
+	{
+		v.push_back(temp_v);
+	}
+	else
+	{
+		for (int i = 1; i <= N; i++)
+		{
+			temp_v.push_back(i);
+			DFS(count + 1);
+			temp_v.pop_back();
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	DFS(0);
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		for (int j = 0; j < v[i].size(); j++)
+		{
+			cout << v[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
