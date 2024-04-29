d47ef7cd353167f3467f0a832e38ba700c14d70b
bon jae ku <scottku@naver.com>
2024-04-29 23:34:53 +0900

    24.04.29 백준 문제풀이 9252 : dp를 활용하는 것은 맞지만, 알맞은 단어까지 출력해줘야 하는 문제. 알맞은 단어를 그때마다 체크해서 저장해간다면 string의 배열 연산으로 인해 시간을 넘겨버린다. 길어질때 마다 dp 값을 더해주는 dp 연산 규칙을 활용해 단어를 역추적해간다.

---
+++ 24.04.29/q9252.cpp
@@ -0,0 +1,76 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+int dp[1001][1001];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s1, s2;
+	int len1, len2;
+	string answer = "";
+
+	cin >> s1;
+	cin >> s2;
+	len1 = s1.length();
+	len2 = s2.length();
+	for (int i = 0; i <= len1; i++)
+	{
+		for (int j = 0; j <= len2; j++)
+			dp[i][j] = 0;
+	}
+
+	for (int i = 1; i <= len1; i++)
+	{
+		for (int j = 1; j <= len2; j++)
+		{
+			if (s1[i - 1] == s2[j - 1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+
+	int a = len1;
+	int b = len2;
+	while (a >= 1 && b >= 1)
+	{
+		if (dp[a][b] > dp[a - 1][b - 1] && dp[a - 1][b - 1] == dp[a - 1][b] && dp[a - 1][b - 1] == dp[a][b - 1])
+		{
+			answer += s2[b - 1];
+			a--;
+			b--;
+		}
+		else if (dp[a][b] == dp[a - 1][b] && dp[a - 1][b] > dp[a][b - 1])
+			a--;
+		else if (dp[a][b] == dp[a][b - 1] && dp[a][b - 1] > dp[a - 1][b])
+			b--;
+		else
+			b--;
+	}
+
+	reverse(answer.begin(), answer.end());
+	if (dp[len1][len2] != 0)
+		cout << dp[len1][len2] << '\n' << answer;
+	else
+		cout << "0";
+}
