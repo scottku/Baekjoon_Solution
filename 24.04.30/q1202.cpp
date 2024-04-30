a26a751921bc9d8b0e8dd71cfc3058e0b42cffba
bon jae ku <scottku@naver.com>
2024-04-30 23:12:58 +0900

    24.04.30 백준 문제풀이 1202 : 어떻게 풀어야 할지는 알지만 센스가 필요했떤 문제 당연히 가능한한 가치가 높은 보섟들만 챙겨가면 그만인 문제지만 해당 가방에 넣을 무게보다 작거나 같은 보석 중 가장 가치가 높은 보석을 찾는 작업은 무게비교, 가치비교 등 반복작업이 여러번 들어가 시간초과를 이끌어낸다. 가방 무게를 낮은것 부터 정렬하여 해당 무게에 넣을 수 있는 보석들 중 가치가 높은 순서대로 보석을 정렬한다면, 그때마다 가장 가치가 높은 보석 하나만 빼버리면 그만이다. 이런 센스가 시간초과를 벗어나는 중요한 것 중 하나로 보여진다.

---
+++ 24.04.30/q1202.cpp
@@ -0,0 +1,81 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+int N, K;
+int M, V;
+int C;
+
+class Comp
+{
+public:
+	bool operator() (const pair<int, int>& p1, const pair<int, int>& p2)
+	{
+		if (p1.second != p2.second)
+			return p1.second > p2.second;
+		else
+			return p1.first > p2.first;
+	}
+};
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> pq;
+priority_queue<int, vector<int>, greater<int>> pq_bag;
+priority_queue<int, vector<int>, less<int>> pq_temp;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> M >> V;
+		pq.push({ V, M });
+	}
+	for (int i = 1; i <= K; i++)
+	{
+		cin >> C;
+		pq_bag.push(C);
+	}
+
+	long long answer = 0;
+	while (!pq_bag.empty())
+	{
+		int bag_weight = pq_bag.top();
+		pq_bag.pop();
+		while (!pq.empty())
+		{
+			if (pq.top().second <= bag_weight)
+			{
+				pq_temp.push(pq.top().first);
+				pq.pop();
+			}
+			else
+				break;
+		}
+		if (!pq_temp.empty())
+		{
+			answer += pq_temp.top();
+			pq_temp.pop();
+		}
+	}
+	cout << answer;
+}
