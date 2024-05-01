1be5f24285f1007e09aec4e2ea21ac5c5cd7c32f
bon jae ku <scottku@naver.com>
2024-05-01 11:56:12 +0900

    24.05.01 백준 문제풀이 10942 : DP를 활용한 문제, 단순이 양 끝이 같은가를 계속해서 비교한다면 최대 계산 횟수는 1000 * 1000000 으로 10억을 넘긴다. 약 10초는 걸리는 방법이므로 시간복잡도가 낮은 방식을 선택해야 하는데, 정렬이 필요한 우선순위 큐는 아닐테고, 투 포인터 방식은 의미가 없어 보이고, 이분탐색도 옳은 방법이 아닌것 같으니 결국은 DP로 돌아오게 된다. 기존에 양 끝이 같은가? 에 대한 물음을 DP로 저장해두면 다음에 다시 확인하러 왔을 때 또 체크할 이유가 없어지니 시간이 단축된다.

---
+++ 24.05.01/q10942.cpp
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N, M;
+int S, E;
+vector<int> v;
+int dp[2001][2001];
+
+int DP(int start, int end)
+{
+	if (end - 1 == start)
+	{
+		if (v[start] == v[end])
+			return dp[start][end] = 1;
+		else
+			return dp[start][end] = 0;
+	}
+
+	if (dp[start][end] != -1)
+		return dp[start][end];
+
+	int n = DP(start + 1, end - 1) + (int)(v[start] == v[end]);
+	if (n == 2) // 둘 다 true
+		return dp[start][end] = 1;
+	else
+		return dp[start][end] = 0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 0; i < 2001; i++)
+	{
+		for (int j = 0; j < 2001; j++)
+		{
+			if (i == j)
+				dp[i][j] = 1;
+			else
+				dp[i][j] = -1;
+		}
+	}
+
+	cin >> N;
+	int temp;
+	v.push_back(0); // 0번째 자리 채워넣기
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	cin >> M;
+	for (int tc = 0; tc < M; tc++)
+	{
+		cin >> S >> E;
+		cout << DP(S, E) << '\n';
+	}
+}
