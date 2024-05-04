598e9bba9aa5048d504fe96da26d159261618b6f
bon jae ku <scottku@naver.com>
2024-05-04 21:20:17 +0900

    24.05.04 백준 문제풀이 1904 : DP 문제, 앞자리가 00 이라면 그 2번째 전의 dp, 1이라면 바로 전의 dp 값과 연관이 있기에 어렵지 않게 구할 수 있다. 참고로 5월 3일은 블루아카이브 GS 콜라보 시작날이었다. 하루종일 편의점으로 빵 찾아 뛰어다니다가 피곤해서 잠들었더니 문제를 못푸는 매우 얼탱이 없는 일이 벌어지고 말았다.

---
+++ 24.05.04/q1904.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+int N;
+int dp[1000001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i <= N; i++)
+	{
+		dp[i] = 0;
+	}
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= N; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 15746; // 1(dp[i-1]) or 00(dp[i-2])
+	}
+	cout << dp[N];
+}
