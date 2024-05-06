a311de37e20b92dcd4c8b8502f254e9c0df29d33
bon jae ku <scottku@naver.com>
2024-05-06 22:41:55 +0900

    24.05.06 백준 문제풀이 1766 : A를 먼저 해야 B를 할 수 있다 = 위상정렬 문제. 큐 혹은 우선순위 큐를 사용해 조건을 만족시킬때마다 큐에 추가하여 작업을 진행하면 된다. 5월부로 3개월간 1문제씩 풀기 달성. 이틀에 한문제 정도로 조정할 예정

---
+++ 24.05.06/q1766.cpp
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+
+
+using namespace std;
+
+int N, M;
+vector<int> arr_v[32001];
+int arr_before[32001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	memset(arr_before, 0, sizeof(arr_before));
+
+	cin >> N >> M;
+
+	int first, second;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> first >> second;
+		arr_before[second]++; // 먼저 풀어야할 문제 갯수 추가
+		arr_v[first].push_back(second);
+	}
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 1; i <= N; i++)
+	{
+		if (arr_before[i] == 0) // 비어있다 == 먼저 풀어야할 문제가 없다.
+			pq.push(i);
+	}
+
+	while (!pq.empty())
+	{
+		int temp = pq.top();
+		pq.pop();
+
+		cout << temp << ' ';
+
+		for (int i = 0; i < arr_v[temp].size(); i++)
+		{
+			int t = --arr_before[arr_v[temp][i]];
+			if (t == 0)
+				pq.push(arr_v[temp][i]);
+		}
+	}
+}
