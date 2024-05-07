bf90dd6a7f835afb7ca4e7483999a923fefb2e98
bon jae ku <scottku@naver.com>
2024-05-07 22:16:12 +0900

    24.05.07 백준 문제풀이 15654 : DFS를 활용한 배열 만들기 문제, 모든 수를 맨 앞자리에 놓고 그 뒤를 작은 순서대로 채워넣는 방식이므로, 반복문을 진행할 때, 특정 수 부터가 아닌 처음부터 시작해야 한다.

---
+++ 24.05.07/q15654.cpp
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <climits>
+#include <time.h>
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+vector<int> v_temp;
+bool visit[9];
+
+void DFS(int index, int count)
+{
+	if (count == M)
+	{
+		for (int i = 0; i < v_temp.size(); i++)
+		{
+			cout << v_temp[i] << ' ';
+		}
+		cout << '\n';
+	}
+	else
+	{
+		for (int i = index; i < v.size(); i++)
+		{
+			if (visit[i])
+				continue;
+			v_temp.push_back(v[i]);
+			visit[i] = true;
+			DFS(0, count + 1);
+			v_temp.pop_back();
+			visit[i] = false;
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	int temp;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+		visit[i] = false;
+	}
+	sort(v.begin(), v.end());
+
+	DFS(0, 0);
+}
